229b0509d932fb5a859f0fcdff74b44ad3642cb6
Aram2003 <aram.martirosyan.y@tumo.org>
2017-09-19 16:29:19 +0400

    some changes

--- collector/index.js
+++ collector/index.js
@@ -3,57 +3,56 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var forEach = require("async-foreach");
 var path = "./data/items.json";
-var url = "https://en.wikipedia.org/wiki/Category:Rockstar_Games";
+var page1 = "https://www.gamespot.com/companies/rockstar-games/?page=1";
+var page2 = "https://www.gamespot.com/companies/rockstar-games/?page=2";
+var page3 = "https://www.gamespot.com/companies/rockstar-games/?page=3";
 var items = [];
 
-request(url, function(error, response, page){
+request(page1, function(error, response, page){
     if(!error){
         var $ = cheerio.load(page);
-        var list = $(".mw-category-group ul li a");
-        for (var i = 13; i<14; i++){
-            console.log($(list[i]).text());
-            console.log($(list[i]).attr("href"));
+        var names = $("h3.media-title");
+        var reldate = $("time.media-date");
+        var description = $("p.media-deck");
             var obj = {
-                id: i,
-                text: $(list[i]).text(),
-                href: $(list[i]).attr("href")
+                name:$(names).text(),
+                reldate:$(reldate).text(),
+                description:$(description).text()
             }
             console.log(obj);
             items.push(obj);
-        }
-        for (var i =17; i<31; i++){
-        console.log($(list[i]).text());
-            console.log($(list[i]).attr("href"));
+    }
+    fs.writeFile(path, JSON.stringify(items));
+});
+request(page2, function(error, response, page){
+    if(!error){
+        var $ = cheerio.load(page);
+        var names = $("h3.media-title");
+        var reldate = $("time.media-date");
+        var description = $("p.media-deck");
             var obj = {
-                id: i,
-                text: $(list[i]).text(),
-                href: $(list[i]).attr("href")
+                name:$(names).text(),
+                reldate:$(reldate).text(),
+                description:$(description).text()
             }
             console.log(obj);
             items.push(obj);
-        }
-        for (var i = 33; i<51; i++){
-        console.log($(list[i]).text());
-            console.log($(list[i]).attr("href"));
+    }
+    fs.writeFile(path, JSON.stringify(items));
+});
+request(page3, function(error, response, page){
+    if(!error){
+        var $ = cheerio.load(page);
+        var names = $("h3.media-title");
+        var reldate = $("time.media-date");
+        var description = $("p.media-deck");
             var obj = {
-                id: i,
-                text: $(list[i]).text(),
-                href: $(list[i]).attr("href")
+                name:$(names).text(),
+                reldate:$(reldate).text(),
+                description:$(description).text()
             }
             console.log(obj);
             items.push(obj);
-        }
-        for(var i=53; i<list.length; i++){
-            console.log($(list[i]).text());
-            console.log($(list[i]).attr("href"));
-            var obj = {
-                id: i,
-                text: $(list[i]).text(),
-                href: $(list[i]).attr("href")
-            }
-            console.log(obj);
-            items.push(obj);
-        }
     }
     fs.writeFile(path, JSON.stringify(items));
 });
